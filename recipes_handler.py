7d7bba8ef6c231ca26ea836a12342fa271638ce9
ElenaGlu <shishalovae@mail.ru>
2024-01-16 16:27:21 +0500

    ok

--- recipes_handler.py
+++ recipes_handler.py
@@ -1,8 +1,7 @@
-import asyncio
-
 import aiohttp
 
 import requests
+
 from aiogram.filters import Command, CommandObject, StateFilter
 from aiogram.types import Message
 from aiogram.utils.formatting import Bold, as_marked_section, as_list
@@ -11,7 +10,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram import Router, types
 
-from utils import recipes
+from utils import recipes, translator
 from utils import detail
 
 router = Router()
@@ -23,38 +22,32 @@ class OrderRecipes(StatesGroup):
 
 
 @router.message(StateFilter(None), Command("category_search_random"))
-async def get_category(message: Message, command: CommandObject, state: FSMContext):
+async def suggest_category(message: Message, command: CommandObject, state: FSMContext):
     if command.args is None:
         await message.answer("Ошибка: не переданы аргументы")
         return
 
-    async with aiohttp.ClientSession():
-        await state.set_data({'count': int(command.args)})
+    await state.set_data({'count': int(command.args)})
 
-        builder = ReplyKeyboardBuilder()
-        response = requests.get('https://www.themealdb.com/api/json/v1/1/list.php?c=list').json()['meals']
-        for d in response:
-            for v in d.values():
-                builder.add(types.KeyboardButton(text=str(v)))
-            builder.adjust(4)
+    builder = ReplyKeyboardBuilder()
+    response = requests.get('https://www.themealdb.com/api/json/v1/1/list.php?c=list').json()['meals']
+    for d in response:
+        for v in d.values():
+            builder.add(types.KeyboardButton(text=str(v)))
+        builder.adjust(4)
 
-        await message.answer(f"Выберите категорию:", reply_markup=builder.as_markup(resize_keyboard=True))
-
-        await state.set_state(OrderRecipes.choosing_category)
+    await message.answer(f"Выберите категорию:", reply_markup=builder.as_markup(resize_keyboard=True))
+    await state.set_state(OrderRecipes.choosing_category)
 
 
 @router.message(OrderRecipes.choosing_category)
-async def get_recipes(message: Message, state: FSMContext):
-    if message is None:
-        await message.answer("Ошибка: не переданы аргументы")
-        return
-
+async def suggest_recipes(message: Message, state: FSMContext):
     async with aiohttp.ClientSession() as session:
-        name = await recipes(session, state, message.text)
+        title_recipes = await recipes(session, state, message.text)
         response = as_list(
             as_marked_section(
                 Bold(f"Как Вам такие варианты:"),
-                *[f'{v}' for v in name]
+                *[f'{v}' for v in title_recipes]
             ),
         )
         kb = [[types.KeyboardButton(text="Покажи рецепты")]]
@@ -62,21 +55,19 @@ async def get_recipes(message: Message, state: FSMContext):
                                              resize_keyboard=True,
                                              )
         await message.answer(**response.as_kwargs(), reply_markup=keyboard)
-
         await state.set_state(OrderRecipes.choosing_recipes)
 
 
 @router.message(OrderRecipes.choosing_recipes)
-async def get_recipes(message: Message, state: FSMContext):
-    if message is None:
-        await message.answer("Ошибка: не переданы аргументы")
-        return
-    await detail(state)
-    content = await state.get_data()
-    response = as_list(
-        as_marked_section(
-            Bold(f"Рецепты:"),
-            *[f'{v}' for v in content.values()]
-        ),
-    )
-    await message.answer(**response.as_kwargs())
+async def send_recipes(message: Message, state: FSMContext):
+    async with aiohttp.ClientSession() as session:
+        await detail(session, state)
+
+    content = (await state.get_data())['content']
+    for dict_recipe in content:
+        v = dict_recipe['meals'][0]
+        title = (translator.translate(v['strMeal'], dest='ru')).text
+        instruction = (translator.translate(v['strInstructions'], dest='ru')).text
+        await message.answer(
+            f"{title}: {instruction}"
+        )
--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import asyncio
 from random import choices
 
-from aiogram.client.session import aiohttp
 from aiogram.fsm.context import FSMContext
 from googletrans import Translator
 
@@ -9,38 +8,35 @@ translator = Translator()
 
 
 async def recipes(session, state: FSMContext, message):
-    user_data = await state.get_data()
-    count = user_data['count']
+    count = (await state.get_data())['count']
     async with session.get(url=f'https://www.themealdb.com/api/json/v1/1/filter.php?c={message}') as resp:
         data = (await resp.json())['meals']
         selection = choices(data, k=count)
-        name_recipe = []
-        id_recipe = []
+        title_recipes = []
+        id_recipes = []
         for d in selection:
             recipe = (translator.translate(d['strMeal'], dest='ru')).text
-            name_recipe.append(recipe)
-            id_recipe.append(d['idMeal'])
+            title_recipes.append(recipe)
+            id_recipes.append(d['idMeal'])
 
-        await state.set_data({'id_recipe': id_recipe})
-        return name_recipe
+        await state.set_data({'id_recipes': id_recipes})
+        return title_recipes
 
 
 async def fetch(session, url):
     async with session.get(url) as resp:
-        # print(resp.status)
         return await resp.json()
 
 
-async def detail(state: FSMContext):
-    user_data = await state.get_data()
-    async with aiohttp.ClientSession() as session:
-        fetch_awaitables = [
+async def detail(session, state: FSMContext):
+    id_recipes = (await state.get_data())['id_recipes']
+    fetch_awaitables = [
             fetch(session,
                   f'https://www.themealdb.com/api/json/v1/1/lookup.php?i={el}')
-            for el in user_data.values()
+            for el in id_recipes
         ]
-        content = await asyncio.gather(*fetch_awaitables)
-        await state.set_data({'content': content})
+    content = await asyncio.gather(*fetch_awaitables)
+    await state.set_data({'content': content})
 
 
 
